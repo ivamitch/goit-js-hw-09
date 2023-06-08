036a7c7fd2792609f6ebf0f27168460d03c8722a
ivamitch <118473358+ivamitch@users.noreply.github.com>
2023-06-08 11:50:36 +0000

    Deploying to gh-pages from @ ivamitch/goit-js-hw-09@1cdb91a15e305decf9da4be50561e32667b3fe6f 🚀

---
+++ 03-promises.dfa500a4.js
@@ -0,0 +1,2 @@
+document.querySelector('input[name="delay"]'),document.querySelector('input[name="step"]'),document.querySelector('input[name="amount"]');document.querySelector(".form").addEventListener("submit",(e=>{e.preventDefault()}));
+//# sourceMappingURL=03-promises.dfa500a4.js.map
